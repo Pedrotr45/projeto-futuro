6ab8bc4b385dce50147616ecb4ed31af0fe86936
Pedrotr45 <ph6878694@gmail.com>
2023-02-14 23:36:58 -0300

    projeto futuro

---
+++ jscript.js
@@ -0,0 +1,29 @@
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+function getDistanceFromTheTop(element) {
+    const id = element.getAttribute("href");
+   return document.querySelector(id).offsetTop;
+}
+
+function nativeScroll(distanceFromTheTop) {
+    window.scroll({
+    
+    top: distanceFromTheTop,
+    behavior: "smooth",
+
+    });
+}
+
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const distanceFromTheTop = getDistanceFromTheTop(event.target) -90;
+    nativeScroll(distanceFromTheTop);
+    
+}
+
+menuLinks.forEach((link) => {
+
+    link.addEventListener("click",scrollToSection);
+
+});
